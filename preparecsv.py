b4a1d8e6286d8094f6f492f9f4c4824c574731c2
Davide Taddei <xp5285@gmail.com>
2020-02-02 17:35:55 +0100

    Updated preparecsv file in order to filter the original train dataset with the top 50 classes with 50 to 100 images [see Google Docs document]

--- preparecsv.py
+++ preparecsv.py
@@ -1,22 +1,53 @@
 import sys, csv
+from collections import Counter
+
+from more_itertools import take
 
 data = csv.reader(open('train.csv'), delimiter=',')
 next(data, None)
 import operator
 
 sortedlist = sorted(data, key=lambda row: int(row[2]), reverse=False)
+counter = 0
+label = -1
+countmap = {}
 
-
-def filterimg(line):
-    if int(line[2]) <= 500:
+def filterimg(el):
+    if 50 <= int(el[1]) <= 100:
         return True
     else:
         return False
 
 
-filteredList = filter(filterimg, sortedlist)
+def filterclass(el):
+    if str(el[2]) in top50dict:
+        return True
+    else:
+        return False
+
+
+for line in sortedlist:
+    if int(line[2] != label):
+        counter = 0
+        label = line[2]
+        countmap[line[2]] = 0
+    else:
+        countmap[line[2]] = countmap[line[2]] + 1
+
+countmapsorted = dict(sorted(countmap.items(), key=operator.itemgetter(1), reverse=True))
+
+filteredList = filter(filterimg, countmapsorted.items())
+top50 = take(50, filteredList)
+top50dict = dict(top50).keys()
+
+with open("train_top50.csv", 'w', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_NONNUMERIC)
+    for line in top50:
+        wr.writerow(line)
+
+train_filter = filter(filterclass, sortedlist)
 
 with open("train_filter.csv", 'w', newline='') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_NONNUMERIC)
-    for line in filteredList:
+    for line in train_filter:
         wr.writerow(line)
